89733ef08017115beddc638cc7b2a95997739d8f
Uier <ap9940506@gmail.com>
2019-03-20 11:28:47 +0800

    upload chatroom

---
+++ main.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+let onlineCount = 0;
+
+app.use(express.static(path.join(__dirname)));
+
+io.on('connection', (socket) => {
+	onlineCount++;
+
+	io.emit('online', onlineCount);
+
+	socket.on('disconnect', () => {
+		onlineCount = (onlineCount < 0 ? 0 : onlineCount-1);
+		io.emit("online", onlineCount);
+	});
+
+	socket.on('send', (msg) => {
+		io.emit("msg", msg);
+	});
+});
+
+server.listen(8888, () => {
+	console.log('server started');
+});
